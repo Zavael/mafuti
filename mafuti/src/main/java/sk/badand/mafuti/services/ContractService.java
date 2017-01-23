67f0411e18c79092e9051a6de5b3ecf6e4b313a7
Zavael <cccp14@atlas.sk>
2017-01-23 22:41:26 +0100

    Contract service concept

---
+++ mafuti/src/main/java/sk/badand/mafuti/services/ContractService.java
@@ -0,0 +1,31 @@
+package sk.badand.mafuti.services;
+
+import sk.badand.mafuti.model.Club;
+import sk.badand.mafuti.model.common.Contract;
+import sk.badand.mafuti.model.common.Person;
+
+import java.util.Optional;
+
+/**
+ * Created by abadinka.
+ */
+public class ContractService {
+
+    //TODO finalize class
+
+    public Contract createContract(Person person, Club club) {
+        return null;
+    }
+
+    public Contract updateContract(Contract contract) {
+        return null;
+    }
+
+    public void finalizeContract() {
+
+    }
+
+    public Optional<Contract> findContract(Person person) {
+        return Optional.empty();
+    }
+}
