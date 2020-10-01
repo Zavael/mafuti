a591d522cde683c33ef738cec2c4904b08d75cad
Zavael <cccp14@atlas.sk>
2020-10-01 16:16:29 +0200

    small enhancements

---
+++ mafuti/src/main/java/sk/badand/mafuti/data/EmptyDataProvider.java
@@ -0,0 +1,33 @@
+package sk.badand.mafuti.data;
+
+import org.apache.commons.collections4.collection.AbstractCollectionDecorator;
+import sk.badand.mafuti.model.Club;
+import sk.badand.mafuti.model.common.Nation;
+import sk.badand.mafuti.model.league.League;
+import sk.badand.mafuti.model.league.LeagueSystem;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class EmptyDataProvider implements DataProvider {
+    @Override
+    public List<Club> getClubs(League league) {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public List<Club> getClubs() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public List<Nation> getNations() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public List<LeagueSystem> getLeagueSystems() {
+        return new ArrayList<>();
+    }
+}
