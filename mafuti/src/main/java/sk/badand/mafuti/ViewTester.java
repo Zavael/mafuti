4fc94080a2659cf8bfec12a25200a701abc988d2
Zavael <cccp14@atlas.sk>
2020-11-26 09:33:55 +0100

    view tester class to debug custom ui in view

---
+++ mafuti/src/main/java/sk/badand/mafuti/ViewTester.java
@@ -0,0 +1,29 @@
+package sk.badand.mafuti;
+
+import com.airhacks.afterburner.views.FXMLView;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import sk.badand.mafuti.ui.newcareer.NewcareerView;
+
+public class ViewTester extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+
+        FXMLView uiView = new NewcareerView();
+//        FXMLView uiView = new CompetitionsView();
+
+        Scene scene = new Scene(uiView.getView(), 1024, 768);
+        primaryStage.initStyle(StageStyle.UTILITY);
+        primaryStage.setTitle("mafuti");
+        scene.getStylesheets().add("/styles/mafuti-light-blue.css");
+//        scene.getStylesheets().add("/styles/mafuti-dark-blue.css");
+//        scene.getStylesheets().add("/styles/JMetroLightTheme.css");
+//        scene.getStylesheets().add("/styles/JMetroDarkTheme.css");
+        primaryStage.centerOnScreen();
+        primaryStage.setResizable(false);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
